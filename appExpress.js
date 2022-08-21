9fb86f296184d266a2c92f7b79424850922f111a
연쁜 <yeon@yeonppeun-ui-MacBookPro.local>
2022-08-21 15:32:24 +0900

    [ADD] 유저 회원가입 엔드포인트 구현

--- appExpress.js
+++ appExpress.js
@@ -1,7 +1,7 @@
 
 const http = require('http')
 const express = require('express')
-const { createUser, createPost } = require('./app')
+const { createUser } = require('./app')
 
 const app = express()
 app.use(express.json())
@@ -11,8 +11,6 @@ app.get('/ping', (req, res) => {
 })
 
 app.post('/signup', createUser)// 첫번째 인자에는 endpoint url 을 기입하고,
-app.post('/posts', createPost)
-app.post('/login', (req, res) => { res.json('login success') }) // 각각의 요청에 대해 핸들링 하는 함수를 두번째 인자로 넣습니다.
 
 const server = http.createServer(app)
 
